5b254fd7464f277187c000417a73a85c10fa4b95
Bharathis28 <bharathis.ece2023@citchennai.net>
2026-04-16 12:46:00 +0530

    refactor: remove obsolete sentry.client.config.ts file

--- sentry.client.config.ts
+++
@@ -1,5 +0,0 @@
-// Compatibility shim for tools that still look for sentry.client.config.ts.
-// Canonical setup lives in instrumentation-client.ts.
-import "./instrumentation-client";
-
-export { onRouterTransitionStart } from "./instrumentation-client";
