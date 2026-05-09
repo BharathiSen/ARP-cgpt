b2fa25ef59507d94499f34df5ca29e13432a6c26
Bharathi Senthilkumar <bharathis.ece2023@citchennai.net>
2026-05-09 23:46:51 +0530

    Remove unnecessary code in instrumentation-client.ts

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,5 +1,4 @@
 import * as Sentry from "@sentry/nextjs";
-
 Sentry.init({
   dsn:
     process.env.NEXT_PUBLIC_SENTRY_DSN ||
@@ -7,5 +6,4 @@ Sentry.init({
   tracesSampleRate: 1.0,
   debug: false,
 });
-
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
